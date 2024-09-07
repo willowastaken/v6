49f2269fbea65adf6a1af732d992e17968ecdaea
willo <153154914+willowastaken@users.noreply.github.com>
2024-09-07 13:11:35 -0500

    Create scriptsInEvents.js

---
+++ games/stickduelbattle/scripts/project/scriptsInEvents.js
@@ -0,0 +1,13 @@
+
+
+
+const scriptsInEvents = {
+
+	async Intro_Event1_Act1(runtime, localVars)
+	{
+		runtime.globalVars.access_token = getWebsite_URL();
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
