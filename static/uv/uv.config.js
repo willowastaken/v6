ae7475cac70a5ab2a233ebf37a92dafee2f3da35
willo <153154914+willowastaken@users.noreply.github.com>
2024-09-08 17:03:47 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,30 +1,10 @@
-const defaultConfig = {
+self.__uv$config = {
     prefix: '/static/pa/',
-    bare: 'https://bonkerbankers-xyz.onrender.com/bare/', 
+    bare:'https://bonkerbankers-xyz.onrender.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
     bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 };
-
-window.addEventListener('load', () => {
-    const savedConfig = localStorage.getItem('bare');
-    if (savedConfig) {
-        defaultConfig.bare = savedConfig;
-    }
-
-    self.__uv$config = defaultConfig;
-
-    initializeUltraviolet();
-});
-
-function initializeUltraviolet() {
-   
-    if (self.__uv$config) {
-        console.log('Using bare link:', self.__uv$config.bare);
-        
-    } else {
-        console.error('Configuration not found');
-    }
-}
