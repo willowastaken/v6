bcdc047257c2b7d04d373a88bcbc5e1252682c9d
willo <153154914+willowastaken@users.noreply.github.com>
2024-09-08 17:09:30 -0500

    Update bare.js

--- static/uv/bare.js
+++ static/uv/bare.js
@@ -1,1 +1,35 @@
+  function updateBare() {
+    var select = document.getElementById('bareSelect');
+    var selectedValue = select.value;
 
+    if (selectedValue === 'custom') {
+      var customBare = prompt("Enter custom Bare URL:", localStorage.getItem('bare'));
+      if (customBare) {
+        saveBare(customBare);
+      }
+    } else {
+      saveBare(selectedValue);
+    }
+  }
+
+  function saveBare(bare) {
+    localStorage.setItem('bare', bare);
+    applyCustomBare();
+  }
+
+  function applyCustomBare() {
+    var bare = localStorage.getItem('bare');
+    if (bare) {
+     
+      self.__uv$config.bare = bare;
+      
+      window.location.reload();
+    }
+  }
+
+  window.onload = function() {
+    var bare = localStorage.getItem('bare');
+    if (bare) {
+      document.getElementById('bareSelect').value = bare;
+    }
+  };
