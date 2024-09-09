2e7717a0bcf5a08cd96e5fa3bc22ae2a48604661
willo <153154914+willowastaken@users.noreply.github.com>
2024-09-08 22:17:06 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/pa/',
-  bare: 'https://bonkerbankers-xyz.onrender.com/bare/',  
+  bare: 'https://pabare.slackersinc.net/bare/',  
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/uv/uv.handler.js',
