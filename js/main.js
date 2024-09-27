6575c16bdf47e5690f56e94df5e67083792be7e4
willo <153154914+willowastaken@users.noreply.github.com>
2024-09-26 20:34:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+{ 
 
+  "name": "PLEXILE ARCADE", 
+
+  "short_name": "PA", 
+
+  "start_url": "/", 
+
+  "display": "standalone", 
+
+  "icons": [{ 
+
+    "src": "/images/plexilearcade.png", 
+
+    "sizes": "512x512", 
+
+    "type": "image/png" 
+
+  }] 
+
+}
